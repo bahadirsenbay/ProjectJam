76b2d81b4b67b0a4a9f682f9743b244d6632ddb3
bahadirsenbay <57575257+bahadirsenbay@users.noreply.github.com>
2023-11-17 01:12:04 +0300

    Added New Updates

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,29 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class CharacterMovements : MonoBehaviour
 {
-    public float speed;
-    Vector2 moveDirection;
-    private Rigidbody2D rb;
+    public float moveSpeed;
+    private Rigidbody2D rb2d;
+    float moveHorizontal;
+    bool facingRight;
+    public float JumpForce;
+    public bool isGrounded;
+    public bool isAttacking = false;
+
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        Debug.Log(SceneManager.GetActiveScene().name);
 
+
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Player Movement
-        float moveX = Input.GetAxisRaw("Horizontal");
-        moveDirection = new Vector2(moveX, rb.velocity.y).normalized;
+        CharacterMove();
+        CharacterAnimation();
+        CharacterJump();
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -38,17 +42,72 @@ public class PlayerController : MonoBehaviour
 
             }
         }
-
     }
 
-    private void FixedUpdate()
+    void CharacterMove()
     {
-        Move();
+        moveHorizontal = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(moveHorizontal * moveSpeed, rb2d.velocity.y);
     }
 
-    void Move()
+    void CharacterAnimation()
     {
-        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+
+
+        if (moveHorizontal > 0 && facingRight == true)
+        {
+            Flip();
+        }
+        if (moveHorizontal < 0 && facingRight == false)
+        {
+            Flip();
+        }
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void CharacterJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (isGrounded)
+            {
+                rb2d.velocity = Vector2.up * JumpForce;
+            }
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Grounded")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Grounded")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Grounded")
+        {
+            isGrounded = false;
+        }
+    }
+
 
 }
